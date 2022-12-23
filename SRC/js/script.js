2f0a41cec50c8201e0c3f67c0f69a5c2efdbe7aa
Davit <davitkartashyan05@gmail.com>
2022-12-23 13:00:18 +0400

    first

---
+++ SRC/js/script.js
@@ -0,0 +1,33 @@
+"Use strict";
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+console.log(4 +  +" - object");
+console.log(4 +  +" 5 ");
+
+let incr = 10,
+    decr = 10;
+
+/* incr++;
+decr--; */
+
+/* ++incr;
+--decr; */
+
+/* console.log(incr++);
+console.log(decr--); */
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+//console.log(2*4 == 8);
+//console.log(2*4 == '8');
+//console.log(2*4 === '8');
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = true;
+      
+console.log(isChecked && isClose);
